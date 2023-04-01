8111ec430f8239a8b7e2a4b4f50cb6f98be47ec8
Carlos563429 <carlitosfg.5051@gmail.com>
2023-03-31 21:12:27 -0600

    Create Youtube Downloader

---
+++ YoutubeDownloader/src/main/java/com/mycompany/youtubedownloader/YoutubeDownloader.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.youtubedownloader;
+
+/**
+ *
+ * @author lenoc
+ */
+public class YoutubeDownloader {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
