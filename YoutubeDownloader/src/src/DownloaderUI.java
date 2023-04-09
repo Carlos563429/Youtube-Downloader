3ed2def5dc6b6ca91d3a2cc8bcf07ee9c66060ac
Carlos563429 <carlitosfg.5051@gmail.com>
2023-04-09 14:31:20 -0600

    Change Java with Maven to Java with Ant

--- YoutubeDownloader/src/src/DownloaderUI.java
+++ YoutubeDownloader/src/src/DownloaderUI.java
@@ -1,15 +1,19 @@
-package source;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package src;
 
 /**
  *
  * @author lenoc
  */
-public class DownloadUI extends javax.swing.JFrame {
+public class DownloaderUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form DownloadUI
+     * Creates new form DownloaderUI
      */
-    public DownloadUI() {
+    public DownloaderUI() {
         initComponents();
     }
 
@@ -55,20 +59,20 @@ public class DownloadUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DownloadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DownloaderUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DownloadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DownloaderUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DownloadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DownloaderUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DownloadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DownloaderUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new DownloadUI().setVisible(true);
+                new DownloaderUI().setVisible(true);
             }
         });
     }
